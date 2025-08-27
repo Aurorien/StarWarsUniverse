313ffecee2af274d74894dd089418166241c196d
Anna Eklund <anna.eklund@yahoo.se>
2025-08-27 12:32:37 +0200

    Add fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import CharacterInfo from "./components/CharacterInfo";
 import CharacterList from "./components/CharacterList";
-import Header from "./Header";
+import Header from "./components/Header";
+import "./App.css";
+import { useEffect, useState } from "react";
+import type { ICharacter, IHomeworld } from "./types";
+import { fetchCharacters, fetchHomeworld } from "./api";
 
 function App() {
+  const [characters, setCharacters] = useState<ICharacter[]>([]);
+  const [selectedCharacter, setSelectedCharacter] = useState<
+    ICharacter | undefined
+  >(undefined);
+  const [homeworld, setHomeworld] = useState<IHomeworld | undefined>(undefined);
+  const [isHomeworldLoading, setIsHomeworldLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchCharacters().then((data) => setCharacters(data));
+  }, []);
+
+  const handleOnCharacterClick = (character: ICharacter) => {
+    setIsHomeworldLoading(true);
+    setSelectedCharacter(characters.find((c) => c.id === character.id));
+
+    fetchHomeworld(character.homeworld).then((homeworld) => {
+      setHomeworld(homeworld);
+      setIsHomeworldLoading(false);
+    });
+  };
   return (
     <>
       <Header />
---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface ICharacter {
+  id: number;
+  name: string;
+  gender: string;
+  skin_color: string;
+  hair_color: string;
+  height: string;
+  eye_color: string;
+  mass: string;
+  homeworld: number;
+  birth_year: string;
+  species_id: number;
+}
+
+export interface IHomeworld {
+  id: number;
+  climate: string;
+  surface_water: string;
+  name: string;
+  diameter: string;
+  rotation_period: string;
+  terrain: string;
+  gravity: string;
+  orbital_period: string;
+  population: string;
+}
